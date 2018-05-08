403ccc9d6aa4b9a873d4f0a0f52c6d87100ca555
sashaoberman <sashaoberman@sashas-mbp>
2018-05-07 23:18:03 -0700

    Created a Day constructor and an addDay() method. Made Background class.

---
+++ HighSchoolExperience/src/Background.java
@@ -0,0 +1,7 @@
+import processing.core.PApplet;
+
+public class Background extends PApplet {
+
+	
+	
+}
--- HighSchoolExperience/src/Day.java
+++ HighSchoolExperience/src/Day.java
@@ -3,4 +3,13 @@ public class Day {
 
 	int day;
 	
+	public Day(int day) {
+		this.day = day;
+	}
+	
+	public void addDay() {
+		day++;
+	}
+	
+	
 }
