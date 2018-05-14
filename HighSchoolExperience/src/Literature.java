64317b277ec315293e76e50df940d63fdeca155b
sashaoberman <sashaoberman@sashas-mbp>
2018-05-13 21:46:00 -0700

    Changed Literature.

--- HighSchoolExperience/src/Literature.java
+++ HighSchoolExperience/src/Literature.java
@@ -1,4 +1,23 @@
+import java.util.ArrayList;
 
-public class Literature {
+public class Literature extends Course {
+
+	private ArrayList<String> litQuestions;
+	
+	public Literature(Grade grade, Teacher teacher) {
+		super(grade, teacher);
+	}
+	
+	public Teacher getLTeacher() {
+		return getTeacher();
+	}
+	
+	public Grade getLGrade() {
+		return getGrade();
+	}
+	
+	public void addLQuestions() {
+		litQuestions = new ArrayList<String>();
+	}
 
 }
